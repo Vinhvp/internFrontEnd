35952ded5fa635931aa6772a14fb2cd9df016f09
Vinhvp <vinh.hcmue.k42tina@gmail.com>
2022-04-06 11:16:46 +0700

    feat:body-homepage-ui

--- src/Pages/Homepage/homepage.jsx
+++ src/Pages/Homepage/homepage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../../Components/Header/Header';
+import Content from '../../Components/Content/Content'
 
 
 const Home = () => {
     return (  
         <>
             <Header></Header>
-           
+            <Content></Content>
         </>
     );
 }
